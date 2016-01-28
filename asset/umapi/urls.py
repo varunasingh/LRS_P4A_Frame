5a00e16cbb729d59af0c78cad2a05764d36db821
Varuna <varuna.singh@gmail.com>
2016-01-28 17:47:38 +0400

    Moved umapi to LRSCode

--- asset/umapi/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('umapi.views',
-    url(r'^$', 'root_view', name='root_view'),
-    url(r'^forward/$', 'forward', name='forward'),
-    url(r'^login/$', 'login', name='umapilogin')
-)
